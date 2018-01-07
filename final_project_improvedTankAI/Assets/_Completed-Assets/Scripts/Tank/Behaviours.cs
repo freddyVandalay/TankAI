377c731b5a92bce436889b2b0831909ec6287613
Fredrik Sundstrom <fsund001@gold.ac.uk>
2018-01-07 22:37:34 +0000

    implemented fwd sensors + obsticle tree

--- final_project_improvedTankAI/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ final_project_improvedTankAI/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -12,6 +12,8 @@ namespace Complete
      */
     public partial class TankAI : MonoBehaviour
     {
+		//private Blackboard ownBlackboard = new Blackboard();
+
         private Root CreateBehaviourTree() {
 
             switch (m_Behaviour) {
@@ -43,6 +45,11 @@ namespace Complete
 		/*Eye of sight*/
 		//Acts like a sensor for the AI tank
 		private void Awareness(){
+			blackboard["obsticleInFront"]= false;
+			blackboard["obsticleInFrontRight"]= false;
+			blackboard["obsticleInFrontLeft"]= false;
+			blackboard["obsticleInFrontLeft"]= false;
+			//blackboard["obsticleInFrontLeft"]= false;
 			//Debug.Log("EYE OF SIGHT METHOD:");
 			Vector3 localPos = this.transform.position;
 			Vector3 obsticle;
@@ -67,15 +74,14 @@ namespace Complete
 
 			//Debug rays
 			Debug.DrawRay(this.transform.position, this.transform.forward * 7, Color.red);
-			Debug.DrawRay(this.transform.position, rightEye * 7, Color.green);
-			Debug.DrawRay(this.transform.position, leftEye * 7, Color.blue);
-			Debug.DrawRay(this.transform.position, -this.transform.forward * 7, Color.black);
-			Debug.DrawRay(this.transform.position, -rightEye * 7, Color.white);
-			Debug.DrawRay(this.transform.position, -leftEye * 7, Color.yellow);
+			Debug.DrawRay(this.transform.position, rightEye * 10, Color.green);
+			Debug.DrawRay(this.transform.position, leftEye * 10, Color.blue);
+			Debug.DrawRay(this.transform.position, -this.transform.forward * 10, Color.black);
+			Debug.DrawRay(this.transform.position, -rightEye * 10, Color.white);
+			Debug.DrawRay(this.transform.position, -leftEye * 10, Color.yellow);
 
 			//Debug.DrawRay(eyesRight * 10, Color.red);
-			if (Physics.Raycast(eyesFwd, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)")
-			{
+			if (Physics.Raycast (eyesFwd, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)") {
 				collHit = hit.point;
 				obsticle = hit.transform.position;
 
@@ -85,11 +91,12 @@ namespace Complete
 				//Debug.Log(collHit.z-obsticle.z);
 
 				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
-				Debug.Log("Too close in front!!" + hit.collider);
-				blackboard["obsticleInFront"]= true;
+				Debug.Log ("Too close in front!!" + hit.collider);
+				blackboard ["obsticleInFront"] = true;
+			} else {
+				blackboard["obsticleInFront"]= false;
 			}
-			if (Physics.Raycast(eyesFwdRight, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)")
-			{
+			if (Physics.Raycast (eyesFwdRight, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)") {
 				collHit = hit.point;
 				obsticle = hit.transform.position;
 
@@ -99,11 +106,12 @@ namespace Complete
 				//Debug.Log(collHit.z-obsticle.z);
 
 				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
-				Debug.Log("Too close to the right!!" + hit.collider);
-				blackboard["obsticleInFront_Right"]= true;
+				Debug.Log ("Too close to the right!!" + hit.collider);
+				blackboard ["obsticleInFrontRight"] = true;
+			} else {
+				blackboard["obsticleInFrontRight"]= false;
 			}
-			if (Physics.Raycast(eyesFwdLeft, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)")
-			{
+			if (Physics.Raycast (eyesFwdLeft, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)") {
 				collHit = hit.point;
 				obsticle = hit.transform.position;
 
@@ -113,8 +121,10 @@ namespace Complete
 				//Debug.Log(collHit.z-obsticle.z);
 
 				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
-				Debug.Log("Too close to the left!!" + hit.collider);
-				blackboard["obsticleInFront_Left"]= true;
+				Debug.Log ("Too close to the left!!" + hit.collider);
+				blackboard ["obsticleInFrontLeft"] = true;
+			} else {
+				blackboard["obsticleInFrontLeft"]= false;
 			}
 			/*
 			if (Physics.Raycast(eyesFwdRight, out hit, 10) && hit.collider.gameObject.name != "CompleteTank(Clone)")
@@ -171,38 +181,75 @@ namespace Complete
 	
 		private Root ImprovedBehaviour() {
 			return new Root(
-				new Service(0.5f, UpdatePerception,
+				new Service(0.2f, UpdatePerception,
 					new Selector(
-
-						new BlackboardCondition("obsticleInFront_Right",Operator.IS_EQUAL,true,Stops.IMMEDIATE_RESTART,
-							new Failer(new Action(() => Turn(-0.5f)))),
-						new BlackboardCondition("obsticleInFront_Left",Operator.IS_EQUAL,true,Stops.IMMEDIATE_RESTART,
-							new Failer(new Action(() => Turn(0.5f)))),
-						new BlackboardCondition("obsticleInFront",Operator.IS_EQUAL,false,Stops.IMMEDIATE_RESTART,
-							new Failer(new Action(() => Turn(0f)))),
-					
-						new Action(() => Move(0.5f))
+						new BlackboardCondition("deadEnd", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
+							new Sequence( new Action(()=> Move(-0.5f)), new Action(() => Turn(0.5f))
+							)
+						),
+						new BlackboardCondition("turnLeft", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
+							new Sequence( new Action(()=> Move(0f)), new Action(() => Turn(-0.5f))
+							)
+						),
+						new BlackboardCondition("turnRight", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
+							new Sequence( new Action(()=> Move(0f)), new Action(() => Turn(0.5f))
+							)
+						),
+						new BlackboardCondition("obsticleInFront", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
+							new Selector(
+								new BlackboardCondition("obsticleOnRight", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
+									new Sequence(new Action (() => Turn(-0.5f)), new Action (() => Move(0f))
+									)
+								),
+								new Action(() => Turn(0.5f))
+							)
+						),
+						new Sequence(new Action(() => Turn(0f)), new Action(() => Move(0.5f))
+						)
 					)
 				)
-			);
+			); 
 		}
 
         private void UpdatePerception() {
-			blackboard["obsticleInFront"]= false;
-			blackboard["obsticleInFrontRight"]= false;
-			blackboard["obsticleInFrontLeft"]= false;
+	
 			Awareness();
-            Vector3 targetPos = TargetTransform().position;
+            
+			Vector3 targetPos = TargetTransform().position;
             Vector3 localPos = this.transform.InverseTransformPoint(targetPos);
             Vector3 heading = localPos.normalized;
             blackboard["targetDistance"] = localPos.magnitude;
             blackboard["targetInFront"] = heading.z > 0;
             blackboard["targetOnRight"] = heading.x > 0;
             blackboard["targetOffCentre"] = Mathf.Abs(heading.x);
-			
+			//Debug.Log (blackboard.Get<bool>("obsticleInFront"));
 			//My additions
-			//blackboard["obsticleOnTheRight"]
-			//blackboard["obsticleOnTheLeft"]
+			if(blackboard.Get<bool>("obsticleInFront") && blackboard.Get<bool>("obsticleInFrontRight") && blackboard.Get<bool>("obsticleInFrontLeft")){
+				blackboard ["turnRight"] = false;
+				blackboard ["turnLeft"] = false;
+				blackboard ["deadEnd"] = true;
+				Debug.Log ("DeadEnd");
+			}
+			if(blackboard.Get<bool>("obsticleInFront") && blackboard.Get<bool>("obsticleInFrontRight") && !blackboard.Get<bool>("obsticleInFrontLeft")){
+				blackboard ["deadEnd"] = false;
+				blackboard ["turnRight"] = false;
+				blackboard ["turnLeft"] = true;
+				Debug.Log ("Left");
+			}
+			if (blackboard.Get<bool> ("obsticleInFront") && !blackboard.Get<bool> ("obsticleInFrontRight") && blackboard.Get<bool> ("obsticleInFrontLeft")) {
+				blackboard ["deadEnd"] = false;
+				blackboard ["turnLeft"] = false;
+				blackboard ["turnRight"] = true;
+				Debug.Log ("Right");
+			} 
+			if (!blackboard.Get<bool> ("obsticleInFront") && !blackboard.Get<bool> ("obsticleInFrontRight") && !blackboard.Get<bool> ("obsticleInFrontLeft")) {
+				blackboard ["deadEnd"] = false;
+				blackboard ["turnLeft"] = false;
+				blackboard ["turnRight"] = false;
+				Debug.Log ("Clear");
+			} 
+
+
 
 			//blackboard["obsticleBehind"]
         }
