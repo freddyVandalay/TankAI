a0e769d7213d22a9d89ecefcf2026b93454a0438
Fredrik Sundstrom <fsund001@gold.ac.uk>
2018-01-08 01:22:02 +0000

    Added comments + structured code

--- final_project_improvedTankAI/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ final_project_improvedTankAI/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -4,12 +4,6 @@ using System.Collections.Generic;
 
 namespace Complete
 {
-    /*
-    Example behaviour trees for the Tank AI.  This is partial definition:
-    the core AI code is defined in TankAI.cs.
-
-    Use this file to specifiy your new behaviour tree.
-     */
     public partial class TankAI : MonoBehaviour
     {
 		//private Blackboard ownBlackboard = new Blackboard();
@@ -19,7 +13,8 @@ namespace Complete
             switch (m_Behaviour) {
 
                 case 1:
-					return ImprovedBehaviour(); 
+					//Enemy travel around the map and seems harmless.
+					return LostBehaviour(); 
 					
 
                 default:
@@ -27,23 +22,19 @@ namespace Complete
             }
         }
 
-        /* Actions */
 
-        private Node StopTurning() {
-            return new Action(() => Turn(0));
-        }
+        /* My Actions */
 
+		//From coursework 1
         private Node RandomFire() {
             return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));
         }
 
+
 		/*My methods*/
 
-		/*Eye of sight*/
-		//Acts like a sensor for the AI tank
+		//Uses 6 sensors to create awareness of the the surrounding.
 		private void Awareness(){
-
-			//blackboard["obsticleInFrontLeft"]= false;
 			//Debug.Log("EYE OF SIGHT METHOD:");
 			Vector3 localPos = this.transform.position;
 			Vector3 obsticle;
@@ -68,6 +59,7 @@ namespace Complete
 
 			//Debug rays
 			Debug.DrawRay(this.transform.position, this.transform.forward * 7, Color.red);
+
 			Debug.DrawRay(this.transform.position, rightEye * 7, Color.green);
 			Debug.DrawRay(this.transform.position, leftEye * 7, Color.blue);
 			Debug.DrawRay(this.transform.position, -this.transform.forward * 7, Color.black);
@@ -83,8 +75,7 @@ namespace Complete
 				//Debug.Log("Object hit: " + obsticle);
 				//Debug.Log("Coll hit: " + collHit);
 				//Debug.Log(collHit.z-obsticle.z);
-
-				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
+			
 				Debug.Log ("Too close in front!!" + hit.collider);
 				blackboard ["obsticleInFront"] = true;
 			} else {
@@ -99,7 +90,6 @@ namespace Complete
 				//Debug.Log("Coll hit: " + collHit);
 				//Debug.Log(collHit.z-obsticle.z);
 
-				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
 				Debug.Log ("Too close to the right!!" + hit.collider);
 				blackboard ["obsticleInFrontRight"] = true;
 			} else {
@@ -114,7 +104,6 @@ namespace Complete
 				//Debug.Log("Coll hit: " + collHit);
 				//Debug.Log(collHit.z-obsticle.z);
 
-				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
 				Debug.Log ("Too close to the left!!" + hit.collider);
 				blackboard ["obsticleInFrontLeft"] = true;
 			} else {
@@ -129,7 +118,6 @@ namespace Complete
 				//Debug.Log("Coll hit: " + collHit);
 				//Debug.Log(collHit.z-obsticle.z);
 
-				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
 				Debug.Log ("Too close to the left!!" + hit.collider);
 				blackboard ["obsticleInBack"] = true;
 			} else {
@@ -145,7 +133,6 @@ namespace Complete
 				//Debug.Log("Coll hit: " + collHit);
 				//Debug.Log(collHit.z-obsticle.z);
 
-				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
 				Debug.Log ("Too close to the left!!" + hit.collider);
 				blackboard ["obsticleInBackRight"] = true;
 			} else {
@@ -161,7 +148,6 @@ namespace Complete
 				//Debug.Log("Coll hit: " + collHit);
 				//Debug.Log(collHit.z-obsticle.z);
 
-				//blackboard["obsticleOnRight"] = collHit.z - obsticle.z > 0;
 				Debug.Log ("Too close to the left!!" + hit.collider);
 				blackboard ["obsticleInBackLeft"] = true;
 			} else {
@@ -173,52 +159,61 @@ namespace Complete
 
         /* My Behaviour Tree */
 
-		private Root ImprovedBehaviour() {
+		private Root LostBehaviour() {
 			return new Root(
 				new Service(0.2f, UpdatePerception,
+					//Steering tree
 					new Selector(
+						//Reached dead end
 						new BlackboardCondition("deadEnd", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
 							new Sequence( 
+								//Stop or reverse
 								new Selector(
 									new NPBehave.Random(0.5f, new Action(() => Move(0f))), 
-									new NPBehave.Random(1f, new TimeMin(1f,new Action(() => Move(-0.5f))) )
-								), 
+									new NPBehave.Random(1f, new TimeMin(1f,new Action(() => Move(-0.3f))) )
+								),
+								//Turn left or right
 								new Selector(
 									new NPBehave.Random(0.5f, new TimeMin(4f, new Action(() => Turn(0.5f)))), 
 									new NPBehave.Random(1f, new TimeMin(4f,new Action(() => Turn(-0.5f))) ) 
 								)
 							)
 						),
+						//Slows down and Perform sharp left turn
 						new BlackboardCondition("turnLeft", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
 							new Sequence( 
-								new Action(()=> Move(0.2f)), 
+								new Action(()=> Move(0.3f)), 
 								new Action(() => Turn(-0.5f))
 							)
 						),
+						//Slows down and perform sharp right turn
 						new BlackboardCondition("turnRight", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
 							new Sequence( 
-								new Action(()=> Move(0.2f)), 
+								new Action(()=> Move(0.3f)), 
 								new Action(() => Turn(0.5f))
 							)
 						),
+						//Obsticle in front turn left or right
 						new BlackboardCondition("obsticleInFront", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
 							new Selector(
 								new TimeMin(0.3f, new NPBehave.Random(0.5f, new Action(() => Turn(-0.5f)))), 
 								new TimeMin(0.3f, new NPBehave.Random(1f, new Action(() => Turn(0.5f))))
 							)
 						),
+						//Perform smooth left turn
 						new BlackboardCondition("smoothTurnLeft", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
 							new Selector(
 								new NPBehave.Random(0.5f, new Action(() => Turn(-0.1f))),new NPBehave.Random(1f, new Action(() => Turn(-0.3f)))
 							)
 						),
+						//Perform smooth right turn
 						new BlackboardCondition("smoothTurnRight", Operator.IS_EQUAL,true, Stops.IMMEDIATE_RESTART,
 							new Selector(
 								new NPBehave.Random(0.5f, new Action(() => Turn(0.1f))),
 								new NPBehave.Random(1f, new Action(() => Turn(0.3f)))
 							)
 						),
-						//Path is clear
+						//Path is clear Go straight ahead
 						new Sequence(
 							new Action(() => Turn(0f)), 
 							new Action(() => Move(0.5f))
@@ -229,9 +224,10 @@ namespace Complete
 		}
 
         private void UpdatePerception() {
-	
+			//Update sensors blackboards
 			Awareness();
             
+			/*From course behaviour script*/
 			Vector3 targetPos = TargetTransform().position;
             Vector3 localPos = this.transform.InverseTransformPoint(targetPos);
             Vector3 heading = localPos.normalized;
@@ -239,10 +235,9 @@ namespace Complete
             blackboard["targetInFront"] = heading.z > 0;
             blackboard["targetOnRight"] = heading.x > 0;
             blackboard["targetOffCentre"] = Mathf.Abs(heading.x);
-			//Debug.Log (blackboard.Get<bool>("obsticleInFront"));
+
 
 			//My additions
-
 			//Path is clear
 			if (!blackboard.Get<bool> ("obsticleInFront") && !blackboard.Get<bool> ("obsticleInFrontRight") && !blackboard.Get<bool> ("obsticleInFrontLeft")) {
 				blackboard ["deadEnd"] = false;
@@ -254,47 +249,52 @@ namespace Complete
 			} 
 			//Deadend reached
 			if(blackboard.Get<bool>("obsticleInFront") && blackboard.Get<bool>("obsticleInFrontRight") && blackboard.Get<bool>("obsticleInFrontLeft")){
+				blackboard ["deadEnd"] = true;
+
 				blackboard ["turnRight"] = false;
 				blackboard ["turnLeft"] = false;
-				blackboard ["deadEnd"] = true;
 				blackboard ["smoothTurnLeft"] = false;
 				blackboard ["smoothTurnRight"] = false;
 				Debug.Log ("DeadEnd");
 			}
 			//Obstiles to the front and to the right
 			if(blackboard.Get<bool>("obsticleInFront") && blackboard.Get<bool>("obsticleInFrontRight") && !blackboard.Get<bool>("obsticleInFrontLeft")){
+				blackboard ["turnLeft"] = true;
+
 				blackboard ["deadEnd"] = false;
 				blackboard ["turnRight"] = false;
-				blackboard ["turnLeft"] = true;
 				blackboard ["smoothTurnLeft"] = false;
 				blackboard ["smoothTurnRight"] = false;
 				Debug.Log ("Left");
 			}
 			//Obstiles to the front and to the left
 			if (blackboard.Get<bool> ("obsticleInFront") && !blackboard.Get<bool> ("obsticleInFrontRight") && blackboard.Get<bool> ("obsticleInFrontLeft")) {
+				blackboard ["turnRight"] = true;
+
 				blackboard ["deadEnd"] = false;
 				blackboard ["turnLeft"] = false;
-				blackboard ["turnRight"] = true;
 				blackboard ["smoothTurnLeft"] = false;
 				blackboard ["smoothTurnRight"] = false;
 				Debug.Log ("Right");
 			} 
 			//Obsticles to the right
 			if (!blackboard.Get<bool> ("obsticleInFront") && blackboard.Get<bool> ("obsticleInFrontRight") && !blackboard.Get<bool> ("obsticleInFrontLeft")) {
+				blackboard ["smoothTurnLeft"] = true;
+
 				blackboard ["deadEnd"] = false;
 				blackboard ["turnLeft"] = false;
 				blackboard ["turnRight"] = false;
-				blackboard ["smoothTurnLeft"] = true;
 				blackboard ["smoothTurnRight"] = false;
 				Debug.Log ("smooth left");
 			}
 			//Obstices to the left
 			if (!blackboard.Get<bool> ("obsticleInFront") && !blackboard.Get<bool> ("obsticleInFrontRight") && blackboard.Get<bool> ("obsticleInFrontLeft")) {
+				blackboard ["smoothTurnRight"] = true;
+
 				blackboard ["deadEnd"] = false;
 				blackboard ["turnLeft"] = false;
 				blackboard ["turnRight"] = false;
 				blackboard ["smoothTurnLeft"] = false;
-				blackboard ["smoothTurnRight"] = true;
 				Debug.Log ("smooth right");
 			}
 			//Obsticles to the left and right
